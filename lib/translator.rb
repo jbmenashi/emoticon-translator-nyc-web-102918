ddf47709abc8401a48755d3851cbd10b7bf51678
Jacob Menashi <jbmenashi@gmail.com>
2018-10-18 19:03:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  emoticons_hash = load_library(file_path)
+  
 end
 
 def get_english_meaning
