ffb98bf9ddfd18691fa09d40fa55500c186e7a60
Jacob Menashi <jbmenashi@gmail.com>
2018-10-18 18:59:54 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,11 @@ def load_library(file_path)
   }
   emoticons =
   YAML.load_file(file_path)
-  
+  emoticons.each do |meaning, emoticons_array|
+    emo_hash["get_meaning"][emoticons_array[1]]= meaning
+    emo_hash["get_emoticon"][emoticons_array[0]]= emoticons_array[1]
+  end
+  emoticons_hash
 end
 
 def get_japanese_emoticon
